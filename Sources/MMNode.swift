087946959958f7eb8aabaac32970e1db6c983880
ampm <roy.yang@sap.com>
2019-06-08 22:44:46 +0800

    update copyright header

--- Sources/MMNode.swift
+++ Sources/MMNode.swift
@@ -1,10 +1,6 @@
-//
-//  MMNode.swift
-//  MappaMundi
-//
-//  Created by Yang, Roy on 2019/6/7.
-//  Copyright © 2019 Mozilla Corporation. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import AStar
 
